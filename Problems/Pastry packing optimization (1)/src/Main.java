02aeaaa4bd09d01ee69ad2d9f606e8e1b3056bae
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-07-31 14:18:08 +0200

    .

--- Problems/Pastry packing optimization (1)/src/Main.java
+++ Problems/Pastry packing optimization (1)/src/Main.java
@@ -51,7 +51,18 @@ class TartBox {
     Hundred more such boring classes OR ...
     magic class for everything everybody is waiting for
 */
-class Box...
+class Box<T> {
+
+    private T type;
+
+    public void put(T type) {
+        this.type = type;
+    }
+
+    public T get() {
+        return this.type;
+    }
+}
 
 // Don't change classes below
 class Cake { }
